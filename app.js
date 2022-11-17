26fdb2be4a74b73a0f5e5dce30540bab07c16a3c
pimdbg <84320205+pimdbg@users.noreply.github.com>
2022-11-17 18:44:12 +0100

    Move createServer() method to app.js file
    Put http library in app.js, and so making ./src/server.js redundant.
    
    Make server listener read desired path from config file, so that it the path is easily configurable for other files as well

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
-const server = require('./src/server');
 const fs = require('fs');
+const http = require('http');
+const { exit } = require('process');
+
 // Read config file
 const config = JSON.parse(fs.readFileSync('config.json', { encoding:'utf8', flag:'r' } ));
 
-// Init server
-server.run();
+
+// Setup Local server
+const server = http.createServer((req, res) => {
+
+    if (req.method !== 'POST') return; // Assure method is post
+    
+    let body = '';
+
+    // Set up response headers
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('');
+    
+    // Handle incoming request
+    req.on('data', (data) => body += data);
+    req.on('end', () => { 
+        const data = JSON.parse(body) ;
+
+        switch(data.action) {
+            case "test": 
+                console.log("Test succesful!");
+                break;
+            case "stream": 
+                console.log("Stream recieved!");
+                break;
+            default: 
+                console.log('No valid action');
+                break;
+        }
+    });
+    
+});
+
+// Listen on server path
+server.listen(port = config.http.port, hostname = config.http.hostname, () => {
+    console.log(`Server running at http://${ hostname }:${ port }/`);
+});
+ 
+
