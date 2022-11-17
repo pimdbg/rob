31dba6cf3a5cc98c1c6386c110de5a025ea8e62c
pimdbg <84320205+pimdbg@users.noreply.github.com>
2022-11-17 18:40:52 +0100

    Read Config file from FileSystem library

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const server = require('./src/server');
+const fs = require('fs');
+// Read config file
+const config = JSON.parse(fs.readFileSync('config.json', { encoding:'utf8', flag:'r' } ));
 
 // Init server
 server.run();
