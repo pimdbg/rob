d4427c65ff6c53479fa74229b7d78942ea0b50f2
pimdbg <84320205+pimdbg@users.noreply.github.com>
2022-11-24 09:38:15 +0100

    Convert VideoCapture frames to Base64

--- py/video.py
+++ py/video.py
@@ -1,20 +1,15 @@
 # import the opencv library
 import cv2
-  
+import base64
   
 # define a video capture object
 vid = cv2.VideoCapture(0)
   
 while(True):
       
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-  
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-
-    print(frame)
+    ret, frame = vid.read()                     # get frame from webcam
+    res, frame = cv2.imencode('.jpg', frame)    # from image to binary buffer
+    data = base64.b64encode(frame)              # convert to base64 format
       
     # the 'q' button is set as the
     # quitting button you may use any
