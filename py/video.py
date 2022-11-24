369610d3ea28cd1e8b0daff01f543fd8ac09df5e
pimdbg <84320205+pimdbg@users.noreply.github.com>
2022-11-24 12:02:55 +0100

    Make video.py file respond with server

--- py/video.py
+++ py/video.py
@@ -1,16 +1,24 @@
 # import the opencv library
 import cv2
 import base64
+import socketio 
   
 # define a video capture object
 vid = cv2.VideoCapture(0)
+
+# Define Socketio client
+sio = socketio.Client()
+sio.connect('http://localhost:3000') # TODO: Make server dynamic to config file
   
 while(True):
       
     ret, frame = vid.read()                     # get frame from webcam
     res, frame = cv2.imencode('.jpg', frame)    # from image to binary buffer
     data = base64.b64encode(frame)              # convert to base64 format
-      
+
+    # Send data to Websocket
+    sio.emit('video-capture', data)
+
     # the 'q' button is set as the
     # quitting button you may use any
     # desired button of your choice
@@ -21,3 +29,5 @@ while(True):
 vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
+# Disconnect Websocket connection
+sio.disconnect()
