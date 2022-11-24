f38d5e46ca55f2044d25b74ec41be951445608b7
pimdbg <84320205+pimdbg@users.noreply.github.com>
2022-11-24 12:02:11 +0100

    Setup Websocket

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
 const fs = require('fs');
-const http = require('http');
+const app = require('express')();
+// const app = express();
 const { exit } = require('process');
+const http = require('http').createServer(app); // Setup server
+const io = require("socket.io")(http); // Web socket
+const path = require('path');
 
 // Read config file
 const config = JSON.parse(fs.readFileSync('config.json', { encoding:'utf8', flag:'r' } ));
 
 
-// Setup Local server
-const server = http.createServer((req, res) => {
 
-    if (req.method !== 'POST') return; // Assure method is post
-    
-    let body = '';
+// App routes
+app.get('/', (req, res) => { // Webcam footage for testing
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
 
-    // Set up response headers
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('');
-    
-    // Handle incoming request
-    req.on('data', (data) => body += data);
-    req.on('end', () => { 
-        const data = JSON.parse(body) ;
+// SocketIO connection
+io.on('connection', (socket) => {
 
-        switch(data.action) {
-            case "test": 
-                console.log("Test succesful!");
-                break;
-            case "stream": 
-                console.log("Stream recieved!");
-                break;
-            default: 
-                console.log('No valid action');
-                break;
-        }
+    /** 
+     * Listen for video capture data
+     * 
+     * @param {string} data - Passed data, expected to be Base64 encoded image file
+     */
+    socket.on('video-capture', (data) => {
+        // Decode data
+        const decoded = Buffer.from(data, 'base64').toString();
+
+        console.log(decoded);
+
+        io.emit('test', decoded); // Pass to test html
     });
-    
-});
 
-// Listen on server path
-server.listen(port = config.http.port, hostname = config.http.hostname, () => {
-    console.log(`Server running at http://${ hostname }:${ port }/`);
-});
- 
 
+})
+
+http.listen(config.http.port, () => {
+    console.log(`Server running on port ${ config.http.port }`)
+})
