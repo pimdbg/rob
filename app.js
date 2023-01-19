9588c4ff0202651f0e18f43fa484fb80dfbca4af
pimdbg <84320205+pimdbg@users.noreply.github.com>
2023-01-19 18:59:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require("express");
-
+const path = require('path');
 const app = express();
 const http = require('http').createServer(app); // Setup server
 
